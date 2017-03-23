bfeddd571fa805945b1c99197c332842af3326ca
Mike J Innes <mike.j.innes@gmail.com>
2017-03-23 02:18:40 +0000

    getindex

--- src/api.jl
+++ src/api.jl
@@ -2,11 +2,21 @@ struct THArray{T,N} #<: AbstractArray{T,N}
   ptr::Ptr{Void}
 end
 
+litsym(xs...) = Expr(:quote, Symbol(xs...))
+
 for N = 1:4
-  @eval function (::Type{THArray{Float64,$N}})(size::NTuple{$N,Integer})
-    ptr = ccall(:THDoubleTensor_newWithSize4d, Ptr{Void},
-                (Clong, Clong, Clong, Clong), $([i ≤ N ? :(size[$i]) : -1 for i = 1:4]...))
-    THArray{Float64,$N}(ptr)
+  @eval begin
+    function (::Type{THArray{Float64,$N}})(size::NTuple{$N,Integer})
+      ptr = ccall(:THDoubleTensor_newWithSize4d, Ptr{Void},
+                  (Clong, Clong, Clong, Clong), $([i ≤ N ? :(size[$i]) : -1 for i = 1:4]...))
+      THArray{Float64,$N}(ptr)
+    end
+    function Base.getindex(xs::THArray{Float64,$N}, idx::Vararg{Integer,$N})
+      @assert all(1 .≤ idx .≤ size(xs))
+      ccall($(litsym(:THDoubleTensor_get, N, :d)), Float64,
+            (Ptr{Void}, $(ntuple(_->Clong, N)...),),
+            xs.ptr, $(ntuple(i->:(idx[$i]-1),N)...))
+    end
   end
 end
 
